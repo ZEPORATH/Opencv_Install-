74706390ee97487cf5b7964aa537cd45300d0542
Zeporath <shekhar.jaigaon@gmail.com>
2017-08-20 20:31:10 +0530

    Updates for installing opencv in home folder

--- install_opencv.sh
+++ install_opencv.sh
@@ -37,10 +37,12 @@ sudo apt-get install -y doxygen
 
 sudo apt-get install -y unzip wget
 wget https://github.com/opencv/opencv/archive/3.2.0.zip
+rm ~/OpenCV
 unzip 3.2.0.zip
 rm 3.2.0.zip
-mv opencv-3.2.0 OpenCV
-cd OpenCV
+mkdir ~/OpenCV
+mv opencv-3.2.0 ~/OpenCV
+cd ~/OpenCV
 mkdir build
 cd build
 cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON ..
